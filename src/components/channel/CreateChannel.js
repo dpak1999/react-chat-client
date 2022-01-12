73519de019a4d2f1bde09096d131084500e0d900
deepak <deepakkumardash313@gmail.com>
2022-01-12 23:11:26 +0530

    add : create channel component

---
+++ src/components/channel/CreateChannel.js
@@ -0,0 +1,49 @@
+/** @format */
+
+import { useState } from 'react';
+import { CloseCreateChannel } from './CloseCreateChannel';
+
+const ChannelNameInput = ({ channelName = '', setChannelName }) => {
+  const handleChange = (e) => {
+    e.preventDefault();
+    setChannelName(e.target.value);
+  };
+
+  return (
+    <div className="channel-name-input__wrapper">
+      <p>Name</p>
+      <input
+        type="text"
+        placeholder="Channel name"
+        value={channelName}
+        onChange={handleChange}
+      />
+      <p>Add Members</p>
+    </div>
+  );
+};
+
+const CreateChannel = ({ createType, setIsCreating }) => {
+  const [channelName, setChannelName] = useState('');
+
+  return (
+    <div className="create-channel__container">
+      <div className="create-channel__header">
+        <p>
+          {createType === 'team'
+            ? 'Create a new channel'
+            : 'Send a direct message'}
+        </p>
+        <CloseCreateChannel setIsCreating={setIsCreating} />
+      </div>
+      {createType === 'team' && (
+        <ChannelNameInput
+          channelName={channelName}
+          setChannelName={setChannelName}
+        />
+      )}
+    </div>
+  );
+};
+
+export default CreateChannel;
